bc9642ec9921333b0ab1af8862a92d3e50010c95
Calvin <loncsime@gmail.com>
2023-02-17 16:14:57 +0100

    New + Fixed files
    Does not compile

---
+++ TestFixes/CountedItem.cs
@@ -0,0 +1,12 @@
+public class CountedItem
+{
+    // Fields
+    public Item TheItem;
+    public int Quantity;
+ 
+    public CountedItem(Item theitem, int quantity)
+    {
+        this.TheItem = theitem;
+        this.Quantity = quantity;
+    }
+}
