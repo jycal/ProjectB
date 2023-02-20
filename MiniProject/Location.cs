32a6620d408768cd20332dab6eaeea533183a943
AryanJadoenath <1061627@hr.nl>
2023-02-20 12:09:26 +0100

    Location

--- MiniProject/Location.cs
+++ MiniProject/Location.cs
@@ -35,6 +35,7 @@ public class Location
         bool IsMoving = true;
         while (IsMoving)
         {
+            var current_location = " ";
             Console.WriteLine("Where would you like to go?");
             Console.WriteLine($"You are at {this.Name}.");
             Console.WriteLine($"  P\n  A\n V F T G B S\n   H");
@@ -42,34 +43,42 @@ public class Location
             if (Direction == "T")
             {
                 World.LocationByID(World.LOCATION_ID_TOWN_SQUARE);
+                current_location += World.LocationByID(World.LOCATION_ID_TOWN_SQUARE);
             }
             if (Direction == "A")
             {
                 World.LocationByID(World.LOCATION_ID_ALCHEMIST_HUT);
+                current_location += World.LocationByID(World.LOCATION_ID_ALCHEMIST_HUT);
             }
             if (Direction == "P")
             {
                 World.LocationByID(World.LOCATION_ID_ALCHEMISTS_GARDEN);
+                current_location += World.LocationByID(World.LOCATION_ID_ALCHEMISTS_GARDEN);
             }
             if (Direction == "G")
             {
                 World.LocationByID(World.LOCATION_ID_GUARD_POST);
+                current_location += World.LocationByID(World.LOCATION_ID_GUARD_POST);
             }
             if (Direction == "B")
             {
                 World.LocationByID(World.LOCATION_ID_BRIDGE);
+                current_location += World.LocationByID(World.LOCATION_ID_BRIDGE);
             }
             if (Direction == "S")
             {
                 World.LocationByID(World.LOCATION_ID_SPIDER_FIELD);
+                current_location += World.LocationByID(World.LOCATION_ID_SPIDER_FIELD);
             }
             if (Direction == "F")
             {
                 World.LocationByID(World.LOCATION_ID_FARMHOUSE);
+                current_location += World.LocationByID(World.LOCATION_ID_FARMHOUSE);
             }
             if (Direction == "V")
             {
                 World.LocationByID(World.LOCATION_ID_FARM_FIELD);
+                current_location += World.LocationByID(World.LOCATION_ID_FARM_FIELD);
             }
             IsMoving = false;
         }
