3e158b971308e0be6ff6d5d4aff1f6478c922d99
JulinaMay <julinamay_@Hotmail.com>
2023-02-22 00:38:07 +0100

    Superadventure&Menu
    Quest nog niet werken, na de superadventure intro ga je naar de menu screen. In Menu heb ik een switch toevegoegd zodat je de locaties kan kiezen.

--- MiniProject/Menu.cs
+++ MiniProject/Menu.cs
@@ -25,44 +25,87 @@ public class Menu
     // Menu
     public void Screen()
     {
-        Console.WriteLine("What wouLd you like to do (Enter a number)?");
-        Console.WriteLine("1: See game stats");
-        Console.WriteLine("2: Move");
-        Console.WriteLine("3: Fight");
-        Console.WriteLine("4: Quit");
-        int choice = Convert.ToInt32(Console.ReadLine());
-        if (choice == 1)
+        bool playerSelected = false;
+        while (!playerSelected)
         {
-            Console.WriteLine($"Your current Location: {CurrentLocation}");
-            Console.WriteLine($"Your current HP is {HP}");
-            Console.WriteLine($"Your at Level: {Level}");
-            Console.WriteLine($"You have {ExperiencePoints} Experience Points");
-            Console.WriteLine($"You have {Gold} Golden Coins");
-            foreach (PlayerQuest quest in QuesList)
+            Console.WriteLine("What wouLd you like to do (Enter a number)?");
+            Console.WriteLine("1: See game stats");
+            Console.WriteLine("2: Move");
+            Console.WriteLine("3: Fight");
+            Console.WriteLine("4: Quit\n");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
             {
-                Console.WriteLine($"The quests you've gone through consist of {quest}");
+                Console.WriteLine($"Your current Location: {CurrentLocation}");
+                Console.WriteLine($"Your current HP is {HP}");
+                Console.WriteLine($"Your at Level: {Level}");
+                Console.WriteLine($"You have {ExperiencePoints} Experience Points");
+                Console.WriteLine($"You have {Gold} Golden Coins");
+                foreach (PlayerQuest quest in QuesList)
+                {
+                    Console.WriteLine($"The quests you've gone through consist of {quest}");
+                }
+                foreach (CountedItemList item in Inventory)
+                {
+                    Console.WriteLine($"Your inventory consists of a {item}");
+                }
             }
-            foreach (CountedItemList item in Inventory)
+            else if (choice == 2)
             {
-                Console.WriteLine($"Your inventory consists of a {item}");
+                Console.WriteLine("Where would you like to go?");
+                Console.WriteLine($"You are at {World.LocationByID(1)}. From here you can go:");
+                Console.WriteLine($"  P\n  A\nV F T G B S\n  H\n");
+                World.LocationByID(1);
+                string destination = Console.ReadLine().ToUpper();
+                switch (destination)
+                {
+                    case "P":
+                        // Move to location P (Alchemist's garden)
+                        this.CurrentLocation = World.LocationByID(5).Name;
+                        Console.WriteLine($"You have arrived at {this.CurrentLocation}.\n");
+                        break;
+                    case "A":
+                        // Move to location A (Alchemist's hut)
+                        this.CurrentLocation = World.LocationByID(4).Name;
+                        Console.WriteLine($"You have arrived at {this.CurrentLocation}.\n");
+
+                        break;
+                    case "V":
+                        // Move to location V (Farmer's field)
+                        break;
+                    case "F":
+                        // Move to location F (Farmer)
+                        break;
+                    case "T":
+                        // Move to location T (Town square)
+                        break;
+                    case "G":
+                        // Move to location G (Guard post)
+                        break;
+                    case "B":
+                        // Move to location B (Bridge)
+                        break;
+                    case "S":
+                        // Move to location S (Spider forest)
+                        break;
+                    case "H":
+                        // Move to location H (Home)
+                        break;
+                    default:
+                        Console.WriteLine("Invalid destination. Please enter a valid location code.");
+                        break;
+                }
             }
-        }
-        else if (choice == 2)
-        {
-            Console.WriteLine("Where would you like to go?");
-            Console.WriteLine($"You are at {CurrentLocation}. From here you can go:");
-            Console.WriteLine($"  P\n  A\n V F T G B S\n   H");
-            World.LocationByID(1);
-        }
-        else if (choice == 3)
-        {
-            // Player en Monster moeten vechten in Game.cs
-            // Player();
-            // Monster();
-        }
-        else if (choice == 4)
-        {
-            Quit();
+            else if (choice == 3)
+            {
+                // Player en Monster moeten vechten in Game.cs
+                // Player();
+                // Monster();
+            }
+            else if (choice == 4)
+            {
+                Quit();
+            }
         }
     }
 
--- MiniProject/SuperAdventure.cs
+++ MiniProject/SuperAdventure.cs
@@ -11,31 +11,39 @@ public class SuperAdventure
         // player aanmaken
         _player = player;
         DateTime now = DateTime.Now;
+        string formattedTime = now.ToString("hh:mm tt");
         string intro = $@"Welcome to Kirby & TheCrystalPrint. Kirby named {player.Name} must find the way to Walter White, the druglord (alchemist).
-Current location: {player.CurrentLocation.Name}, it is currently {now.Hour}:{now.Minute}
-Time to get out of bed.";
+Current location: {player.CurrentLocation.Name}, it is currently {formattedTime}
+Time to get out of bed.
+";
         foreach (char c in intro)
         {
             Console.Write(c);
             Thread.Sleep(50);
         }
         Console.WriteLine();
-        // game over op false zetten
-        _gameOver = false;
+        Menu menuScreen = new Menu("Title", "Description", 1, 2, 3, 4);
+        menuScreen.Screen();
+        // World.PopulateLocations();
+        // Menu.Screen();
+        // // game over op false zetten
+        // _gameOver = false;
     }
 
     // methods
+
     // game loop
     public void Run()
     {
         // game loop
-        while (!_gameOver)
-        {
-            // game loop
-            Console.WriteLine("Game loop!!!!!!!!!");
-            // string Discription = World.LocationByID(1).Description;
-            // Console.WriteLine(Discription);
-            _gameOver = true;
-        }
+
+        // while (!_gameOver)
+        // {
+        //     // game loop
+        //     Console.WriteLine("Game loop!!!!!!!!!");
+        //     // string Discription = World.LocationByID(1).Description;
+        //     // Console.WriteLine(Discription);
+        //     _gameOver = true;
+        // }
     }
 }
